f656430cb29bec0bf659394d3ce1d70e53aff6e9
joco23 <jocoblue@yahoo.com>
2021-09-01 22:34:14 +0200

    Initial commit

---
+++ src/com/vezbi3/Main.java
@@ -0,0 +1,24 @@
+package com.vezbi3;
+
+public class Main {
+
+    public static void main(String[] args) {
+	System.out.println("New Begining");
+    }
+    public static void sayAdios(String name){
+        System.out.println("Adios " + name);
+     sayAdios("Amigo");
+
+
+    }
+
+    @Override
+    public String toString() {
+        return "Main{}";
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        return super.equals(obj);
+    }
+}
